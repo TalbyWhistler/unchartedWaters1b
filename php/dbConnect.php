dbd067d057a845598a16d38b02cc621e6da1525f
TalbyWhistler <000748591@mohawkcollege.ca>
2026-05-10 02:23:45 -0400

    frontAndBackendTalkStartDb

---
+++ php/dbConnect.php
@@ -0,0 +1,31 @@
+<?php 
+     
+
+function screenPrint($inputString)
+{
+    echo $inputString;
+}
+
+
+$servername="localhost";
+//$servername="%waters";
+$username = "webUser1";
+$password = "watersWeb";
+$dbName = "unchartedWatersb";
+
+$conn = new mysqli($servername,$username,$password,$dbName);
+
+if ($conn->connect_error)
+    {
+        echo "false";
+    }
+    else 
+        {
+            echo "true";
+        }
+
+
+echo "true";
+
+
+?>
